98bca138791ecc19f1dec24b8d818998d67f6310
EmersonDrako <epena28@misena.edu.co>
2017-06-11 16:17:40 -0500

    camara

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,47 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        //document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+
+capturePhone:function(){
+    navigator.camera.getPicture(onSuccess,onFail,{
+        quality:50,
+        destinationType: Camera.DestinationType.DATA_URL,
+        saveToPhotoAlbum:true});
+    function onSuccess(imageURI){
+        var image= document.getElementById('minhaImagem');
+        image.src= "data:image/jpeg;base64,"+imageURI;
+    }
+    function onFail(message){
+        alert('FAILED BECAUSE'+message);
+    }
+}
+
+/*
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+*/
+};
